c3c9b7be39bd2f3b2e59b406f2500c3d70532513
ialihaider75 <ialihaider75@gmail.com>
2026-09-03 23:39:46 +0500

    feat: add library-owned KeycloakIntegrationException

---
+++ src/main/java/com/intellermatrix/keycloak/exception/KeycloakErrorReason.java
@@ -0,0 +1,9 @@
+package com.intellermatrix.keycloak.exception;
+
+public enum KeycloakErrorReason {
+    COMMUNICATION_ERROR,
+    USER_NOT_FOUND,
+    INVALID_CREDENTIALS,
+    INVALID_REQUEST,
+    INTERNAL_SERVER_ERROR
+}
